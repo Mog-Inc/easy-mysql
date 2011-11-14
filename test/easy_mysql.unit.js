e0fa0e6dfa33076ce9c07aad7906130b6791e88c
Bryan Donovan <bdondo@gmail.com>
2011-11-14 11:00:08 -0800

    using explicit connect, connect_with_pool, and connect_with_easy_pool class methods instead of a single create method

--- test/easy_mysql.unit.js
+++ test/easy_mysql.unit.js
@@ -3,194 +3,25 @@ var EasyMySQL = require('../lib/easy_mysql');
 var easy_pool = require('../lib/easy_pool');
 var common    = require('./common');
 var settings  = common.settings;
+var clone     = common.clone;
 var check_err = common.check_err;
 var setup_db  = common.setup_db;
 
 module.exports = testCase({
-    setUp: function (callback) {
-        var self = this;
-        setup_db(function (err, result) {
-            check_err(err);
-            self.easy = new EasyMySQL(settings);
-            callback();
-        });
-    },
-
-    "execute": testCase({
-        "valid queries": testCase({
-            "with params": function (test) {
-                var self = this;
-                var sql = "insert into widgets(name) values (?)";
-
-                self.easy.execute(sql, ['foo'], function (err, result) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.ok(result);
-                    var sql = "select * from widgets";
-                    self.easy.execute(sql, function (err, results) {
-                        test.equal(results[0].name, 'foo');
-                        test.done();
-                    });
-                });
-            },
-
-            "without params": function (test) {
-                var self = this;
-                var sql = "insert into widgets(name) values ('bob')";
-
-                self.easy.execute(sql, function (err, result) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.ok(result);
-                    var sql = "select * from widgets";
-                    self.easy.execute(sql, function (err, results) {
-                        test.equal(results[0].name, 'bob');
-                        test.done();
-                    });
-                });
-            }
-        }),
-
-        "invalid query": testCase({
-            "calls callback with error": function (test) {
-                var self = this;
-                var sql = "BOGUSselect * from widgets where id = ?";
-                self.easy.execute(sql, [1], function (err, result) {
-                    test.ok(err instanceof Error);
-                    test.strictEqual(result, null);
-                    test.done();
-                });
-            }
-        })
-    }),
-
-    "get_one": testCase({
+    "instance methods": testCase({
         setUp: function (callback) {
             var self = this;
-            var sql = "insert into widgets(name) values ('bob'), ('jim')";
-            self.easy.execute(sql, function (err, result) {
+            setup_db(function (err, result) {
                 check_err(err);
+                self.easy = EasyMySQL.connect(settings);
                 callback();
             });
         },
 
-        "valid queries": testCase({
-            "with params": function (test) {
-                var self = this;
-                var sql = "select * from widgets where name = ?";
-                self.easy.get_one(sql, ['bob'], function (err, result) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.equal(result.name, 'bob');
-                    test.done();
-                });
-            },
-
-            "without params": function (test) {
-                var self = this;
-                var sql = "select name from widgets order by name desc limit 1";
-                self.easy.get_one(sql, function (err, result) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.equal(result.name, 'jim');
-                    test.done();
-                });
-            },
-
-            "no results - returns null": function (test) {
-                var self = this;
-                var sql = "select * from widgets where name = ?";
-                self.easy.get_one(sql, ['not real'], function (err, result) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.strictEqual(result, null);
-                    test.done();
-                });
-            }
-        }),
-
-        "invalid query": testCase({
-            "calls callback with error": function (test) {
-                var self = this;
-                var sql = "BOGUSselect * from widgets";
-                self.easy.get_one(sql, function (err, result) {
-                    test.ok(err instanceof Error);
-                    test.strictEqual(result, null);
-                    test.done();
-                });
-            }
-        })
-    }),
-
-    "get_all": testCase({
-        setUp: function (callback) {
-            var self = this;
-            var sql = "insert into widgets(name) values ('bob'), ('jim')";
-            self.easy.execute(sql, function (err, result) {
-                check_err(err);
-                callback();
-            });
-        },
-
-        "valid queries": testCase({
-            "with params": function (test) {
-                var self = this;
-                var sql = "select * from widgets where name = ?";
-                self.easy.get_all(sql, ['bob'], function (err, results) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.ok(Array.isArray(results));
-                    test.equal(results[0].name, 'bob');
-                    test.done();
-                });
-            },
-
-            "without params": function (test) {
-                var self = this;
-                var sql = "select name from widgets order by name desc";
-                self.easy.getAll(sql, function (err, results) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.ok(Array.isArray(results));
-                    test.equal(results.length, 2);
-                    test.equal(results[0].name, 'jim');
-                    test.done();
-                });
-            },
-
-            "no results - returns empty array": function (test) {
-                var self = this;
-                var sql = "select * from widgets where name = ?";
-                self.easy.get_all(sql, ['not real'], function (err, results) {
-                    check_err(err, test);
-                    test.strictEqual(err, null);
-                    test.ok(Array.isArray(results));
-                    test.equal(results.length, 0);
-                    test.done();
-                });
-            }
-        }),
-
-        "invalid query": testCase({
-            "calls callback with error": function (test) {
-                var self = this;
-                var sql = "BOGUSselect * from widgets";
-                self.easy.get_all(sql, function (err, results) {
-                    test.ok(err instanceof Error);
-                    test.strictEqual(results, null);
-                    test.done();
-                });
-            }
-        })
-    }),
-
-    "passing in a generic pool object": testCase({
         "execute": testCase({
             "valid queries": testCase({
                 "with params": function (test) {
                     var self = this;
-                    var pool = easy_pool.fetch(settings);
-                    self.easy = new EasyMySQL({pool: pool});
                     var sql = "insert into widgets(name) values (?)";
 
                     self.easy.execute(sql, ['foo'], function (err, result) {
@@ -203,8 +34,200 @@ module.exports = testCase({
                             test.done();
                         });
                     });
+                },
+
+                "without params": function (test) {
+                    var self = this;
+                    var sql = "insert into widgets(name) values ('bob')";
+
+                    self.easy.execute(sql, function (err, result) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.ok(result);
+                        var sql = "select * from widgets";
+                        self.easy.execute(sql, function (err, results) {
+                            test.equal(results[0].name, 'bob');
+                            test.done();
+                        });
+                    });
+                }
+            }),
+
+            "invalid query": testCase({
+                "calls callback with error": function (test) {
+                    var self = this;
+                    var sql = "BOGUSselect * from widgets where id = ?";
+                    self.easy.execute(sql, [1], function (err, result) {
+                        test.ok(err instanceof Error);
+                        test.strictEqual(result, null);
+                        test.done();
+                    });
+                }
+            })
+        }),
+
+        "get_one": testCase({
+            setUp: function (callback) {
+                var self = this;
+                var sql = "insert into widgets(name) values ('bob'), ('jim')";
+                self.easy.execute(sql, function (err, result) {
+                    check_err(err);
+                    callback();
+                });
+            },
+
+            "valid queries": testCase({
+                "with params": function (test) {
+                    var self = this;
+                    var sql = "select * from widgets where name = ?";
+                    self.easy.get_one(sql, ['bob'], function (err, result) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.equal(result.name, 'bob');
+                        test.done();
+                    });
+                },
+
+                "without params": function (test) {
+                    var self = this;
+                    var sql = "select name from widgets order by name desc limit 1";
+                    self.easy.get_one(sql, function (err, result) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.equal(result.name, 'jim');
+                        test.done();
+                    });
+                },
+
+                "no results - returns null": function (test) {
+                    var self = this;
+                    var sql = "select * from widgets where name = ?";
+                    self.easy.get_one(sql, ['not real'], function (err, result) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.strictEqual(result, null);
+                        test.done();
+                    });
+                }
+            }),
+
+            "invalid query": testCase({
+                "calls callback with error": function (test) {
+                    var self = this;
+                    var sql = "BOGUSselect * from widgets";
+                    self.easy.get_one(sql, function (err, result) {
+                        test.ok(err instanceof Error);
+                        test.strictEqual(result, null);
+                        test.done();
+                    });
+                }
+            })
+        }),
+
+        "get_all": testCase({
+            setUp: function (callback) {
+                var self = this;
+                var sql = "insert into widgets(name) values ('bob'), ('jim')";
+                self.easy.execute(sql, function (err, result) {
+                    check_err(err);
+                    callback();
+                });
+            },
+
+            "valid queries": testCase({
+                "with params": function (test) {
+                    var self = this;
+                    var sql = "select * from widgets where name = ?";
+                    self.easy.get_all(sql, ['bob'], function (err, results) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.ok(Array.isArray(results));
+                        test.equal(results[0].name, 'bob');
+                        test.done();
+                    });
+                },
+
+                "without params": function (test) {
+                    var self = this;
+                    var sql = "select name from widgets order by name desc";
+                    self.easy.getAll(sql, function (err, results) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.ok(Array.isArray(results));
+                        test.equal(results.length, 2);
+                        test.equal(results[0].name, 'jim');
+                        test.done();
+                    });
+                },
+
+                "no results - returns empty array": function (test) {
+                    var self = this;
+                    var sql = "select * from widgets where name = ?";
+                    self.easy.get_all(sql, ['not real'], function (err, results) {
+                        check_err(err, test);
+                        test.strictEqual(err, null);
+                        test.ok(Array.isArray(results));
+                        test.equal(results.length, 0);
+                        test.done();
+                    });
+                }
+            }),
+
+            "invalid query": testCase({
+                "calls callback with error": function (test) {
+                    var self = this;
+                    var sql = "BOGUSselect * from widgets";
+                    self.easy.get_all(sql, function (err, results) {
+                        test.ok(err instanceof Error);
+                        test.strictEqual(results, null);
+                        test.done();
+                    });
                 }
             })
         })
+    }),
+
+    "connecting with pools": testCase({
+        setUp: function (callback) {
+            var self = this;
+            setup_db(function (err, result) {
+                check_err(err);
+                callback();
+            });
+        },
+
+        "EasyMysql.connect_with_pool": function (test) {
+            var pool = easy_pool.fetch(settings);
+            var easy = EasyMySQL.connect_with_pool(pool);
+            var sql  = "insert into widgets(name) values (?)";
+
+            easy.execute(sql, ['foo'], function (err, result) {
+                check_err(err, test);
+                test.strictEqual(err, null);
+                test.ok(result);
+                var sql = "select * from widgets";
+                easy.execute(sql, function (err, results) {
+                    test.equal(results[0].name, 'foo');
+                    test.done();
+                });
+            });
+        },
+
+        "EasyMysql.connect_with_easy_pool": function (test) {
+            var easy_pool_settings = clone(settings);
+            var easy = EasyMySQL.connect_with_easy_pool(easy_pool_settings);
+            var sql  = "insert into widgets(name) values (?)";
+
+            easy.execute(sql, ['foo'], function (err, result) {
+                check_err(err, test);
+                test.strictEqual(err, null);
+                test.ok(result);
+                var sql = "select * from widgets";
+                easy.execute(sql, function (err, results) {
+                    test.equal(results[0].name, 'foo');
+                    test.done();
+                });
+            });
+        }
     })
 });
