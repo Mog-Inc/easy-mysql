d9b819654a312002c2f2d6c087439031b461de2c
Bryan Donovan <bdondo@gmail.com>
2011-12-28 17:54:08 -0800

    converted tests to mocha

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 PACKAGE = easy_mysql
 NODEJS = $(if $(shell test -f /usr/bin/nodejs && echo "true"),nodejs,node)
 BASE = .
+
+all: test lint doc
+
 test:
-	./node_modules/nodeunit/bin/nodeunit --reporter nested test/easy_mysql.unit.js test/easy_client.unit.js
+	./node_modules/.bin/mocha
 
 lint:
-	./node_modules/jshint/bin/hint lib test --config $(BASE)/.jshintrc && echo "Lint Done"
+	./node_modules/.bin/jshint lib test --config $(BASE)/.jshintrc && echo "Lint Done"
 	
 doc:
 	./node_modules/jsdoc/app/run.js -t=./node_modules/jsdoc/templates/jsdoc -d=./doc ./lib
--- test/easy_client.unit.js
+++ test/easy_client.unit.js
@@ -1,4 +1,4 @@
-var testCase   = require('nodeunit').testCase;
+var assert     = require('assert');
 var sinon      = require('sinon');
 var EasyClient = require('../lib/easy_client');
 var easy_pool  = require('../lib/easy_pool');
@@ -8,85 +8,90 @@ var clone      = common.clone;
 var check_err  = common.check_err;
 var setup_db   = common.setup_db;
 
-module.exports = testCase({
-    setUp: function (callback) {
-        var self = this;
+describe("EasyClient", function () {
+    beforeEach(function (done) {
         setup_db(function (err, result) {
             check_err(err);
-            callback();
+            done();
         });
-    },
+    });
 
-    "EasyClient.fetch": testCase({
-        "directly": function (test) {
-            EasyClient.fetch(settings, function (err, easy_client) {
-                check_err(err, test);
-                test.equal(easy_client.client.user, settings.user);
-                test.equal(easy_client.client.password, settings.password);
-                test.equal(easy_client.client.port, settings.port);
-                test.equal(easy_client.client.database, settings.database);
-                easy_client.end();
-                test.done();
-            });
-        },
-
-        "passing in a generic pool object": function (test) {
-            var pool = easy_pool.fetch(settings);
-            EasyClient.fetch({pool: pool}, function (err, easy_client) {
-                check_err(err, test);
-                test.equal(easy_client.client.user, settings.user);
-                test.equal(easy_client.client.password, settings.password);
-                test.equal(easy_client.client.port, settings.port);
-                test.equal(easy_client.client.database, settings.database);
-                easy_client.end();
-                test.done();
-            });
-        },
-
-        "using built-in pool": function (test) {
-            var _settings = clone(settings);
-            _settings.pool_size = 3;
-            _settings.use_easy_pool = true;
-            EasyClient.fetch(_settings, function (err, easy_client) {
-                check_err(err, test);
-                test.equal(easy_client.client.user, settings.user);
-                test.equal(easy_client.client.password, settings.password);
-                test.equal(easy_client.client.port, settings.port);
-                test.equal(easy_client.client.database, settings.database);
-                test.ok(easy_client.pool);
-                easy_client.end();
-                test.done();
-            });
-        }
-    }),
-
-    "end() method": testCase({
-        "single client": testCase({
-            "disconnects from socket": function (test) {
+    describe("EasyClient.fetch", function () {
+        describe("directly", function () {
+            it("sets up single client with supplied settings", function (done) {
                 EasyClient.fetch(settings, function (err, easy_client) {
-                    check_err(err, test);
+                    check_err(err);
+                    assert.equal(easy_client.client.user, settings.user);
+                    assert.equal(easy_client.client.password, settings.password);
+                    assert.equal(easy_client.client.port, settings.port);
+                    assert.equal(easy_client.client.database, settings.database);
+                    easy_client.end();
+                    done();
+                });
+            });
+        });
+
+        describe("passing in a generic pool object", function () {
+            it("uses clients from supplied pool", function (done) {
+                var pool = easy_pool.fetch(settings);
+                EasyClient.fetch({pool: pool}, function (err, easy_client) {
+                    check_err(err);
+                    assert.equal(easy_client.client.user, settings.user);
+                    assert.equal(easy_client.client.password, settings.password);
+                    assert.equal(easy_client.client.port, settings.port);
+                    assert.equal(easy_client.client.database, settings.database);
+                    easy_client.end();
+                    done();
+                });
+            });
+        });
+
+        describe("using built-in pool", function () {
+            it("sets up pool and uses its clients", function (done) {
+                var _settings = clone(settings);
+                _settings.pool_size = 3;
+                _settings.use_easy_pool = true;
+                EasyClient.fetch(_settings, function (err, easy_client) {
+                    check_err(err);
+                    assert.equal(easy_client.client.user, settings.user);
+                    assert.equal(easy_client.client.password, settings.password);
+                    assert.equal(easy_client.client.port, settings.port);
+                    assert.equal(easy_client.client.database, settings.database);
+                    assert.ok(easy_client.pool);
+                    easy_client.end();
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("end() method", function () {
+        describe("when using single client", function () {
+            it("disconnects from socket", function (done) {
+                EasyClient.fetch(settings, function (err, easy_client) {
+                    check_err(err);
                     easy_client.end();
 
                     setTimeout(function () {
-                        test.strictEqual(easy_client.client._socket.destroyed, true);
+                        assert.strictEqual(easy_client.client._socket.destroyed, true);
                     }, 20);
-                    test.done();
+                    done();
                 });
-            }
-        }),
+            });
+        });
 
-        "generic pool object": testCase({
-            "releases client back to pool": function (test) {
+        describe("when using generic pool object", function () {
+            it("releases client back to pool", function (done) {
                 var pool = easy_pool.fetch(settings);
                 var mock = sinon.mock(pool);
                 mock.expects("release");
                 EasyClient.fetch({pool: pool}, function (err, easy_client) {
-                    check_err(err, test);
+                    check_err(err);
                     easy_client.end();
                     mock.verify();
-                    test.done();
+                    done();
                 });
-            }
-        })
-    })
+            });
+        });
+    });
 });
